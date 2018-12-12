b56a5c5146f1c6103eead12857b9c9dbfe021379
karthikeyana <karthikeyan.a@live.com>
2018-12-12 17:14:08 +0530

    mixed content browser warning fixed. To add meta properties in head

---
+++ build/precache-manifest.6381982ed7af2c37f257c6601c26fa28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/My-Library/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/My-Library/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/My-Library/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "15f81cc41c2359b61195",
+    "url": "/My-Library/static/js/runtime~main.15f81cc4.js"
+  },
+  {
+    "revision": "18a0aee021ae447ff302",
+    "url": "/My-Library/static/js/main.18a0aee0.chunk.js"
+  },
+  {
+    "revision": "462dcbd4b0002075eafc",
+    "url": "/My-Library/static/js/1.462dcbd4.chunk.js"
+  },
+  {
+    "revision": "18a0aee021ae447ff302",
+    "url": "/My-Library/static/css/main.53d6a1e2.chunk.css"
+  },
+  {
+    "revision": "4e0a8287470ea4331c1359a47e4c84bc",
+    "url": "/My-Library/index.html"
+  }
+];
